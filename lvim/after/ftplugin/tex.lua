bc0d1582bedd62c2df36c78c18c2a3a871274c6e
Swapneel Pathak <swapiitr@gmail.com>
2022-11-23 09:06:17 +0000

    Add settings for `tex` files

---
+++ lvim/after/ftplugin/tex.lua
@@ -0,0 +1,11 @@
+vim.bo.shiftwidth = 2
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 2
+vim.bo.expandtab = true
+vim.bo.textwidth = 0
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.bo.textwidth = 80
+vim.bo.spelllang = "en_us"
+vim.cmd("set spell")
+vim.cmd("set colorcolumn=80")
